2a3e15b8e92ba22cc16855a658654c2e2cdedcb2
Simernor <simonnorenius@gmail.com>
2022-11-21 00:32:37 +0100

    added js-functionality

---
+++ java.js
@@ -0,0 +1,8 @@
+function revealText(){
+    document.getElementById("hidden").style.display = "block";
+    
+}
+
+function hideText(){
+    document.getElementById("hidden").style.display = "none";
+}
