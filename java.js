7c4fd4ad39ae7e4143532391be5b3fe1e52a7c8f
Simernor <simonnorenius@gmail.com>
2022-12-05 00:32:57 +0100

    fixed code

--- java.js
+++ java.js
@@ -2,15 +2,3 @@ function revealText(){
     document.getElementById("left").classList.add("visibility");
     document.getElementById("right").classList.add("visibility");
 }
-
-// transform: translateX(-100%);
-// transform: translateX(0);
-// filter: blur(8px);
-
-// getElementsByClassName("name") alla element med en viss klass​
-
-// let btn = document.getElementById('btn');​
-// btn.addEventListener('click', function(event) {​ detta händer varje gång användaren klickar på knappen​
-// });
-
-// element.style.backgroundColor = 'hotpink';​​
